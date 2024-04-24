d954c96ec8866aad54855f80102a1f603d0fa8be
datnguyentb <datnguyen.github@gmail.com>
2024-04-24 22:18:32 +0700

    update

--- assets/source/index.js
+++ assets/source/index.js
@@ -1,7 +1,9 @@
 const deg = 6;
-const hr = document.querySelector('#hr');
-const mn = document.querySelector('#mn');
-const sc = document.querySelector('#sc');
+const hr = document.querySelector('.clock-1 #hr');
+const mn = document.querySelector('.clock-1 #mn');
+const sc = document.querySelector('.clock-1 #sc');
+const time_clock_2 = document.querySelector('.clock-2 .hour h1');
+
 
 setInterval(() => {
   let date = new Date();
@@ -13,4 +15,5 @@ setInterval(() => {
   hr.style.transform = `rotate(${hh + mm/12}deg)`;
   mn.style.transform = `rotate(${mm}deg)`;
   sc.style.transform = `rotate(${ss}deg)`;
+  time_clock_2.textContent = `${date.getHours()<10?'0'+date.getHours():date.getHours()}:${date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes()}:${date.getSeconds()<10?'0' + date.getSeconds():date.getSeconds()}`
 })
